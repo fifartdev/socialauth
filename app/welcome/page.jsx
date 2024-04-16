1d097601bc7d2f996e422b6000b4d88b3e7630f7
fifartdev <fifartapp@gmail.com>
2024-04-16 16:57:33 +0300

    pre-pushing-3

--- app/welcome/page.jsx
+++ app/welcome/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import { Client, Account } from "appwrite";
-import { redirect, useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 const client = new Client();
 
