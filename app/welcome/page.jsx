a66e27576e08c690fd84f301d5e69cf0761a74ce
fifartdev <fifartapp@gmail.com>
2024-04-16 16:50:54 +0300

    pre-pushing-n

--- app/welcome/page.jsx
+++ app/welcome/page.jsx
@@ -43,7 +43,7 @@ if(session)
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
       <img src='https://fifart.net/wp-content/uploads/2019/04/logo-website.png' className='max-w-32'/>
-      WelcomePage
+      Welcome Page
       <button onClick={logOut}>Logout</button>
       </div></main>
       
